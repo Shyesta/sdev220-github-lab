843c13f32120d5c984674831323152edc9ce0535
Sage Bain <bain.sage@student.greenriver.edu>
2023-06-09 16:37:27 -0700

    Added documentation to the project.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+/**
+ * This is the entry point to my sample app.
+ *
+ * @author Sage Bain
+ * @version 1.0
+ */
 public class Main
 {
+    /**
+     * Starts the application
+     * @param args command-line args (not used)
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello world, from SDEV220!");
